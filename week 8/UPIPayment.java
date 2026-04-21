dd7ef5cc595a50322c8469d48783c785551df251
rainboestrykr <abhirajbhowmick27@gmail.com>
2026-04-21 23:18:47 +0530

    fixed

---
+++ week 8/UPIPayment.java
@@ -0,0 +1,10 @@
+public class UPIPayment implements PaymentGateway {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " via UPI");
+    }
+    @Override
+    public void refund(double amount) {
+        System.out.println("Refunded " + amount + " to UPI");
+    }
+}
